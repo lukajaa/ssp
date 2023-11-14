5d4c2db60a61c7d5b0bb980419aed967d98f3516
lukajaa <lucaskchang@gmail.com>
2023-11-14 11:14:27 -0800

    autoplay video

--- components/Essay.vue
+++ components/Essay.vue
@@ -1,11 +1,12 @@
 <template>
-  <div class="min-h-screen p-2 font-serif text-lg lg:p-10">
+  <div class="p-2 font-serif text-lg lg:p-10">
     <div class="mx-auto overflow-hidden rounded-lg lg:w-2/3">
       <video
         class="video h-full md:-mt-24 md:h-auto md:w-full"
         autoplay
-        controls
+        muted
         loop
+        controls
       >
         <source
           src="https://ia800701.us.archive.org/4/items/MarketStreet19064KScan20181016/MarketStreet_4K_to_2K_cropped_higher_contrast.mp4"
