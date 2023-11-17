586ff2fdc941425ee2d2e7ea272d70717d848cfd
lukajaa <lucaskchang@gmail.com>
2023-11-17 11:29:01 -0800

    change map

--- components/Map.vue
+++ components/Map.vue
@@ -10,8 +10,8 @@
     ]"
   >
     <LTileLayer
-      url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}.png"
-      attribution='&copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       layer-type="base"
       name="OpenStreetMap"
     />
