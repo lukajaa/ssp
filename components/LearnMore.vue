b245fa4d0cfe27da2c0b7638d7a5876ba653411a
Lucas Chang <lchang@MacBook-Pro.local>
2024-02-22 10:23:27 -0800

    sf board

--- components/LearnMore.vue
+++ components/LearnMore.vue
@@ -3,15 +3,13 @@
     <p class="text-center text-7xl font-bold tracking-tight">Take Action</p>
     <img src="~/assets/svgs/underline_1.svg" class="mx-auto" alt="underline" />
     <div class="mt-6 flex flex-wrap px-8">
-      <div class="flex w-full flex-col p-4 md:w-1/2">
+      <div class="flex w-full flex-col p-4 md:w-1/3">
         <div class="h-full rounded ring-4 ring-black">
           <div class="font-bubble p-4">
             <p class="text-3xl font-bold text-blue-500">Local Organizations</p>
             <p class="text-black">
               Joining local organizations is a great way to get involved in your
               community and make a difference.
-              <!-- They often provide opportunities
-              to volunteer and resources to learn more about local issues. -->
             </p>
             <NuxtLink to="/organizations">
               <UButton color="blue" class="mt-4 font-sans">
@@ -21,7 +19,7 @@
           </div>
         </div>
       </div>
-      <div class="flex w-full flex-col p-4 md:w-1/2">
+      <div class="flex w-full flex-col p-4 md:w-1/3">
         <div class="h-full rounded ring-4 ring-black">
           <div class="font-bubble p-4">
             <p class="text-3xl font-bold text-yellow-500">Legislation</p>
@@ -34,6 +32,40 @@
                 Learn More
               </UButton>
             </NuxtLink>
+          </div>
+        </div>
+      </div>
+      <div class="flex w-full flex-col p-4 md:w-1/3">
+        <div class="h-full rounded ring-4 ring-black">
+          <div class="font-bubble p-4">
+            <p class="text-3xl font-bold text-blue-500">Elected Officials</p>
+            <p class="text-black">
+              Reach out to your elected officials to share your thoughts and
+              concerns about local issues.
+            </p>
+            <div class="space-x-2">
+              <UButton
+                to="https://sfbos.org/"
+                color="blue"
+                class="mt-4 font-sans"
+              >
+                SF Board
+              </UButton>
+              <UButton
+                to="https://sfbos.org/meetings"
+                color="blue"
+                class="mt-4 font-sans"
+              >
+                Meeting Schedule
+              </UButton>
+              <UButton
+                to="https://sfbos.org/index.aspx?page=193"
+                color="blue"
+                class="mt-4 font-sans"
+              >
+                Committees
+              </UButton>
+            </div>
           </div>
         </div>
       </div>
